abb49805f0907d2d7a996d3f79d763bede9f555d
P.A.G.N.  Rathnaweeera <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2025-02-18 12:07:48 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main {
+    public static void main(String[] args) {
+        int arr1[] = new int[] {1, 2, 3, 5};
+        int arr2[] = new int[arr1.length];
+
+        // Corrected line
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i]; // Changed 'arr' to 'arr1'
+        }
+
+        System.out.println("Elements of original array: ");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+
+        System.out.println();
+
+        // Displaying elements of array arr2
+        System.out.println("Elements of new array: ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
+}
